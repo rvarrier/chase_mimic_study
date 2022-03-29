3ae5e6a115d52060e60c2f90f785312d04642e7e
Rekha S. Varrier <f0053cz@discovery7.hpcc.dartmouth.edu>
2022-03-29 16:22:06 -0400

    jspsych setup

---
+++ jspsych_implementation/chase_mimic_study/2_devices_off.js
@@ -0,0 +1,6 @@
+
+devices_off = [
+    {
+      "devices_off": "<p>The survey has been optimized for Google Chrome. The videos may not play in other internet browsers (e.g. Safari) - meaning that you may not be able to submit the survey, so please switch to Chrome before you begin.<br><br>. This task requires full attention for about 30 minutes. At this stage, you should:<br>(1) Mute your phone and email notifications, and make sure that there are no other distractions for the next 30 minutes<br>(2) Make sure you are seated comfortably, and <br>(3) Wear glasses or contact lenses if you use them. You will primarily need the mouse, so make sure that the controls work well. Once you ensure that everything is fine, please close all other browsers and proceed.</p>"
+    }
+   ]
